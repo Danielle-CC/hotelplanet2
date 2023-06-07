cdcc6282ef7302b9945df8979a69278baf1c2433
Danielle-CC <113472043+Danielle-CC@users.noreply.github.com>
2023-06-07 16:50:08 +0100

    Various Changes
    Changes to Featured Destination, CSS to Hotel Planet Logo Text, Search Buttons

--- root/js/script.js
+++
@@ -1,9 +0,0 @@
-var min = new Date(),
-    max = new Date(min.getFullYear(), min.getMonth() + 6, min.getDate());
-                            
-mobiscroll.datepicker('#dates', {
-    controls: ['calendar'],
-    select: 'range',
-    min: min,
-    max: max
-})
